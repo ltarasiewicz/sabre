ba27d29b37450c33a6452ce22b41497d45be5bc2
ltarasiewicz <lukasz.tarasiewicz@polcode.net>
2015-06-26 16:12:20 +0200

    Start configuring the theme, add plugins as a submodule

---
+++ config/deploy/staging.rb
@@ -0,0 +1,27 @@
+set :stage, :staging
+
+# Simple Role Syntax
+# ==================
+#role :app, %w{deploy@example.com}
+#role :web, %w{deploy@example.com}
+#role :db,  %w{deploy@example.com}
+
+# Extended Server Syntax
+# ======================
+server '92.222.75.38', user: 'web', roles: %w{web app db}
+
+# you can set custom ssh options
+# it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
+# you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
+# set it globally
+#  set :ssh_options, {
+#    keys: %w(~/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
+
+fetch(:default_env).merge!(wp_env: :staging)
+
+set :wpcli_remote_url, 'http://sabre.tarasiewicz.net/'
+set :wpcli_local_url, 'http://sabre.local/'
+set :wpcli_remote_uploads_dir, '/srv/www/sabre.tarasiewicz.net/shared/uploads/'
