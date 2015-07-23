55fb9e9cab4f5e6924319888f2a90d1dc0c1f7e1
ltarasiewicz <lukasz.tarasiewicz@polcode.net>
2015-07-23 22:01:32 +0200

    Convert the site to full width

--- web/app/themes/sabre/base.php
+++ web/app/themes/sabre/base.php
@@ -18,7 +18,7 @@ use Roots\Sage\Wrapper;
       do_action('get_header');
       get_template_part('templates/header');
     ?>
-    <div class="wrap container" id="<?php if('brand' === get_post_type()) { echo 'brand-page'; } ?>" role="document">
+    <div class="wrap container-fluid" id="<?php if('brand' === get_post_type()) { echo 'brand-page'; } ?>" role="document">
       <div class="content row ">
         <main class="main" role="main">
           <?php include Wrapper\template_path(); ?>
